e050979dbe55fc784d9a4bffebceddd872c46751
Asad Khan <asad_khan47@hotmail.co.uk>
2019-01-15 14:22:23 +0000

    Refactored setting a new balance into a private method

--- lib/account.rb
+++ lib/account.rb
@@ -11,22 +11,18 @@ class Account
   def deposit(money, dep_transaction = Transactions.new(@balance))
     deposit_transaction = dep_transaction
     deposit_transaction.credit(money)
-    @balance = deposit_transaction.balance
+    set_new_balance(deposit_transaction.balance)
   end
 
   def withdraw(money, with_transaction = Transactions.new(@balance))
     withdrawal_transaction = with_transaction
     withdrawal_transaction.debit(money)
-    @balance = withdrawal_transaction.balance
+    set_new_balance(withdrawal_transaction.balance)
   end
 
   private
 
-  def add_money_to_balance(money)
-    @balance += money
-  end
-
-  def subtract_money_from_balance(money)
-    @balance -= money
+  def set_new_balance(money)
+    @balance = money
   end
 end
