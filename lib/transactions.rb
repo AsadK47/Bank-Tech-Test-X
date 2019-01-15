c2577637ef29cde417624081f3619e2d6674e664
Asad Khan <asad_khan47@hotmail.co.uk>
2019-01-15 12:51:01 +0000

    Refactored for improved readability for transaction as debit type

--- lib/transactions.rb
+++ lib/transactions.rb
@@ -10,12 +10,16 @@ class Transactions
   end
 
   def debit
-    @type = 'debit'
+    type_is_debit
   end
 
   private
 
   def type_is_credit
     @type = 'credit'
+  end
+
+  def type_is_debit
+    @type = 'debit'
   end
 end
