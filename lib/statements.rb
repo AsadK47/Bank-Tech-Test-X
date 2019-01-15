7383a5487e2499152ea62299b3593eb920a64e41
Asad Khan <asad_khan47@hotmail.co.uk>
2019-01-15 16:48:22 +0000

    Added spec and file for print_out in statements

--- lib/statements.rb
+++ lib/statements.rb
@@ -8,4 +8,25 @@ class Statements
   def add(transactions)
     @list << transactions
   end
+
+  def print_out
+    @list.reverse!
+
+    header = 'date || credit || debit || balance'
+
+    @list.each do |transaction|
+      header += "\n"
+      header += "#{transaction.date} || "
+      if "#{transaction.type}" == 'credit'
+        header += "#{transaction.amount} || ||"
+      elsif "#{transaction.type}" == 'debit'
+        header += "|| #{transaction.amount} ||"
+      else
+        "|| "
+      end
+      header += " #{transaction.balance}"
+    end
+    return header
+  end
+
 end
--- spec/statements_spec.rb
+++ spec/statements_spec.rb
@@ -2,7 +2,8 @@ require 'statements'
 
 describe Statements do
   let(:statements) { described_class.new }
-  let(:transactions) { double :transactions }
+  let(:transactions_1) { double :transactions_1 }
+  let(:transactions_2) { double :transactions_2 }
 
   it 'initializes with an empty list of transactions' do
     expect(statements.list).to eq([])
@@ -10,8 +11,20 @@ describe Statements do
 
   describe '#add' do
     it 'adds transactions to the array' do
-      statements.add(transactions)
-      expect(statements.list).to include(transactions)
+      statements.add(transactions_1)
+      expect(statements.list).to include(transactions_1)
+    end
+  end
+
+  describe '#print' do
+    it 'prints out the transactions' do
+      allow(transactions_1).to receive(:type).and_return('credit')
+      allow(transactions_1).to receive(:date).and_return('15/01/2019')
+      allow(transactions_1).to receive(:amount).and_return('1000.00')
+      allow(transactions_1).to receive(:balance).and_return('1000.00')
+      statements.add(transactions_1)
+
+      expect(statements.print_out).to eq("date || credit || debit || balance\n15/01/2019 || 1000.00 || || 1000.00")
     end
   end
 end
